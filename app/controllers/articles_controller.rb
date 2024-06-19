5dfb1dd41d305da3c7bde8e0b634d11cd87957cc
SoCalDave90278 <david@digitallifecyclemarketing.com>
2024-06-19 16:23:09 -0700

    adding Ruby Project
    trying to add to git

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,21 +2,25 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
+
   def create
-    @artice = Article.new(title: "...", body: "...")
-  
+    @article = Article.new(article_params)
+
     if @article.save
-      redirect_to @article 
+      redirect_to @article
     else
-      render :new, status: :unprocessible_entity
+      render :new, status: :unprocessable_entity
     end
   end
+
   def edit
     @article = Article.find(params[:id])
   end
@@ -29,6 +33,13 @@ class ArticlesController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
