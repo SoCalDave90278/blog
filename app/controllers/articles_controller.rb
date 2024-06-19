55a31e627efe689461128c51a432c0696a6dea15
SoCalDave90278 <david@digitallifecyclemarketing.com>
2024-06-19 07:42:14 -0700

    First Ruby Project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @artice = Article.new(title: "...", body: "...")
+  
+    if @article.save
+      redirect_to @article 
+    else
+      render :new, status: :unprocessible_entity
+    end
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
